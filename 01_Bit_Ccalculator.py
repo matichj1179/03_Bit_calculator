efc295b8a812d382b4898c4232ef620f9dfed14e
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2022-05-13 10:34:31 +1200

    Add files via upload

---
+++ 01_Bit_Ccalculator.py
@@ -0,0 +1,19 @@
+# Functions go here
+
+# Puts series of symbols at the start and end of text (for emphasis)
+def statement_generator(text, decoration):
+
+    # Make string with five characters
+    ends = decoration * 5
+
+    #  add decoration to start and end of statements
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print ()
+    print(statement)
+    print ()
+
+    return ""
+
+# Main routine goes here
+statement_generator("hello world", "-")
