ff83e4420776bf61ca5f4ff04998ae7add75cecc
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2022-06-02 12:05:45 +1200

    Add files via upload

--- 01_Bit_Ccalculator.py
+++ 01_Bit_Ccalculator.py
@@ -60,8 +60,7 @@ def num_check(question, low):
     valid = False
     while not valid:
 
-        error = "please enter a interger that is more than "
-        "(or eqaul to) {}".format(low)
+        error = "please enter a interger that is more than (or eqaul to) {}".format(low)
 
         try:
             response = int(input(question))
